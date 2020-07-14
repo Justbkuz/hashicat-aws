2ad3e4e7753e176c1cf7b480cd67ecac103ee68e
Stephen Zuk <Justbkuz@users.noreply.github.com>
2020-07-14 19:09:28 +0000

    fix vpc

--- vpc.tf
+++ vpc.tf
@@ -1,8 +1,14 @@
-resource "aws_vpc" "main" {
-  cidr_block       = "10.0.0.0/16"
-  instance_tenancy = "default"
-
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+  name = "zuk-vpc"
+  cidr = "10.0.0.0/16"
+  azs             = ["us-east-1a"]
+  private_subnets = ["10.0.1.0/24"]
+  public_subnets  = ["10.0.101.0/24"]
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
   tags = {
-    Name = "main"
+    Terraform = "true"
+    Environment = "dev"
   }
 }
